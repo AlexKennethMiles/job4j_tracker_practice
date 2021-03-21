5f78f55c37ea6297ac5d9b3b9f89a17252c34c33
AlexKennethMiles <apuchkov17@gmail.com>
2021-03-21 08:10:55 +0300

    1.6. Взаимодействие объектов [#443140]

---
+++ src/main/java/ru/job4j/oop/Wolf.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Wolf {
+    public void eat(Girl girl) {
+    }
+
+    public void tryEat(Ball ball) {
+        System.out.println("Волк послушал песню, но колобка не съел");
+    }
+}
