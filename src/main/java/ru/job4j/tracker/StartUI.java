2af549bc26b2e608ea2d2b9f57806d9100a5392e
AlexKennethMiles <apuchkov17@gmail.com>
2021-03-24 01:13:11 +0300

    5. Tracker - хранилище [#443165]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,12 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        /*DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateFormat = item.getCreated().format(formatter);
-        System.out.println(currentDateFormat);
+        System.out.println(currentDateFormat);*/
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+        System.out.println(tracker.findById(1));
+
     }
 }
