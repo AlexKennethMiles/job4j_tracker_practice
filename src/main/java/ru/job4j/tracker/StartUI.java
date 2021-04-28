583cad12c55fbf60de875f2133efb10c7d17d74a
AlexKennethMiles <apuchkov17@gmail.com>
2021-04-29 01:00:37 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#443112]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,14 +12,13 @@ public class StartUI {
         while (run) {
             this.showMenu(actions);
             int select = input.askInt("Select: ");
-            while (select < 0 || select > actions.length) {
-//                System.out.println("!!!! Error! Incorrect menu item selected! !!!!");
-                select = input.askInt("Select: ");
+            if (select < 0 || select >= actions.length) {
+                output.println("Wrong input, you can select: 0 .. " + (actions.length - 1));
+                continue;
             }
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
-
     }
 
     private void showMenu(UserAction[] actions) {
@@ -30,7 +29,7 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Output output = new ConsoleOutput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
