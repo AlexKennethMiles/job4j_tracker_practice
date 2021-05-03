ed758ff7e9b9cf067ee0d838e45cf94334af5404
AlexKennethMiles <apuchkov17@gmail.com>
2021-05-03 12:32:05 +0300

    0.5.(1) Иерархия исключений и множественный catch. [#443108]

--- src/test/java/ru/job4j/ex/UserStoreTest.java
+++ src/test/java/ru/job4j/ex/UserStoreTest.java
@@ -2,17 +2,13 @@ package ru.job4j.ex;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import static ru.job4j.ex.UserStore.findUser;
-import static ru.job4j.ex.UserStore.validate;
-
 public class UserStoreTest {
     @Test(expected = UserNotFoundException.class)
     public void whenUserWithNameNotFound() throws UserNotFoundException {
         User[] users = {
                 new User("Petr Arsentev", true)
         };
-        findUser(users, "Ivan Ivanov");
+        UserStore.findUser(users, "Ivan Ivanov");
     }
 
     @Test(expected = UserInvalidException.class)
@@ -20,7 +16,7 @@ public class UserStoreTest {
         User[] users = {
                 new User("Ya", true)
         };
-        validate(users[0]);
+        UserStore.validate(users[0]);
     }
 
     @Test(expected = UserInvalidException.class)
@@ -28,6 +24,6 @@ public class UserStoreTest {
         User[] users = {
                 new User("Anatoliy", false)
         };
-        validate(users[0]);
+        UserStore.validate(users[0]);
     }
 }
