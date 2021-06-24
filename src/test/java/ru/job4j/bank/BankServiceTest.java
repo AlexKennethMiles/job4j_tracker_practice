db0e5c590d9544ca20b4cf1ff52138dd0a5aa49f
AlexKennethMiles <apuchkov17@gmail.com>
2021-06-25 01:37:57 +0300

    3. Банковские переводы [#10038]

--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -16,6 +16,16 @@ public class BankServiceTest {
     }
 
     @Test
+    public void addDuplicateUser() {
+        User prime = new User("3434", "Petr Arsentev");
+        User dup = new User("3434", "Ivan Ivanov");
+        BankService bank = new BankService();
+        bank.addUser(prime);
+        bank.addUser(dup);
+        assertThat(bank.findByPassport("3434"), is(prime));
+    }
+
+    @Test
     public void whenEnterInvalidPassport() {
         User user = new User("3434", "Petr Arsentev");
         BankService bank = new BankService();
@@ -34,6 +44,16 @@ public class BankServiceTest {
     }
 
     @Test
+    public void addDuplicateAccount() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.getPassport(), new Account("5546", 4000D));
+        assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));
+    }
+
+    @Test
     public void transferMoney() {
         User user = new User("3434", "Petr Arsentev");
         BankService bank = new BankService();
@@ -41,16 +61,45 @@ public class BankServiceTest {
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
         bank.addAccount(user.getPassport(), new Account("113", 50D));
         bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
+        assertThat(bank.findByRequisite(user.getPassport(), "5546").getBalance(), is(0D));
         assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
     }
 
     @Test
-    public void add2Accounts() {
+    public void transferMoneyBetweenTwoUsers() {
+        User first = new User("3434", "Petr Arsentev");
+        User second = new User("6765", "Ivan Ivanov");
+        BankService bank = new BankService();
+        bank.addUser(first);
+        bank.addUser(second);
+        bank.addAccount(first.getPassport(), new Account("123", 150D));
+        bank.addAccount(second.getPassport(), new Account("875", 300D));
+        bank.transferMoney(first.getPassport(), "123", second.getPassport(), "875", 150D);
+        assertThat(bank.findByRequisite("3434", "123").getBalance(), is(0D));
+        assertThat(bank.findByRequisite("6765", "875").getBalance(), is(450D));
+    }
+
+    @Test
+    public void failedTransfer() {
+        User first = new User("3434", "Petr Arsentev");
+        User second = new User("6765", "Ivan Ivanov");
+        BankService bank = new BankService();
+        bank.addUser(first);
+        bank.addUser(second);
+        bank.addAccount(first.getPassport(), new Account("123", 30D));
+        bank.addAccount(second.getPassport(), new Account("875", 70D));
+        bank.transferMoney(first.getPassport(), "123", second.getPassport(), "875", 40D);
+        assertThat(bank.findByRequisite("3434", "123").getBalance(), is(30D));
+        assertThat(bank.findByRequisite("6765", "875").getBalance(), is(70D));
+    }
+
+    @Test
+    public void theSameAccountWhenTransferring() {
         User user = new User("3434", "Petr Arsentev");
         BankService bank = new BankService();
         bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("1111", 150D));
-        bank.addAccount(user.getPassport(), new Account("1122", 300D));
-        assertThat(bank.findByRequisite("3434", "1122").getBalance(), is(300D));
+        bank.addAccount(user.getPassport(), new Account("123", 100D));
+        bank.transferMoney(user.getPassport(), "123", user.getPassport(), "123", 100D);
+        assertThat(bank.findByRequisite("3434", "123").getBalance(), is(100D));
     }
 }
