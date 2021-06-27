ee9375136fcff33950b3d7803f5f805468b7585a
AlexKennethMiles <apuchkov17@gmail.com>
2021-06-27 22:57:39 +0300

    4. Сортировка номера [#221284]

---
+++ src/test/java/ru/job4j/collection/LexSortTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class LexSortTest {
+    @Test
+    public void sortNum1and2and10() {
+        String[] input = {
+                "10. Task.",
+                "1. Conditions and task.",
+                "2. Addition to the task."
+        };
+        String[] out = {
+                "1. Conditions and task.",
+                "2. Addition to the task.",
+                "10. Task."
+        };
+        Arrays.sort(input, new LexSort());
+        assertThat(input, is(out));
+    }
+}
