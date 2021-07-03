4c0cbe275c098054aca0304870d7cd6e1252e024
AlexKennethMiles <apuchkov17@gmail.com>
2021-07-03 19:13:23 +0300

    1.1.(1) Анонимные классы [#214142 #173470]

--- src/test/java/ru/job4j/lambda/AttachmentSortTest.java
+++ src/test/java/ru/job4j/lambda/AttachmentSortTest.java
@@ -11,49 +11,49 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AttachmentSortTest {
-//    @Test
-//    public void whenSimpleAbstractComparatorForSize() {
-//        List<Attachment> attachments = new ArrayList<>(Arrays.asList(
-//                new Attachment("image 3", 13),
-//                new Attachment("image 1", 100),
-//                new Attachment("image 2", 34))
-//        );
-//        List<Attachment> expected = new ArrayList<>(Arrays.asList(
-//                new Attachment("image 3", 13),
-//                new Attachment("image 2", 34),
-//                new Attachment("image 1", 100))
-//        );
-//        Comparator comparator = new Comparator() {
-//            @Override
-//            public int compare(Object o1, Object o2) {
-//                Attachment left = (Attachment) o1;
-//                Attachment right = (Attachment) o2;
-//                return left.getSize() - right.getSize();
-//            }
-//        };
-//        attachments.sort(comparator);
-//        assertThat(attachments, is(expected));
-//    }
-//
-//    @Test
-//    public void whenGeneralizedAbstractComparatorForName() {
-//        List<Attachment> attachments = new ArrayList<>(Arrays.asList(
-//                new Attachment("image 3", 79),
-//                new Attachment("image 1", 42),
-//                new Attachment("image 2", 34))
-//        );
-//        List<Attachment> expected = Arrays.asList(
-//                new Attachment("image 1", 42),
-//                new Attachment("image 2", 34),
-//                new Attachment("image 3", 79)
-//        );
-//        Comparator comparatorGen = new Comparator<Attachment>() {
-//            @Override
-//            public int compare(Attachment o1, Attachment o2) {
-//                return o1.getName().compareTo(o2.getName());
-//            }
-//        };
-//        attachments.sort(comparatorGen);
-//        assertThat(attachments, is(expected));
-//    }
+    @Test
+    public void whenSimpleAbstractComparatorForSize() {
+        List<Attachment> attachments = new ArrayList<>(Arrays.asList(
+                new Attachment("image 3", 13),
+                new Attachment("image 1", 100),
+                new Attachment("image 2", 34))
+        );
+        List<Attachment> expected = new ArrayList<>(Arrays.asList(
+                new Attachment("image 3", 13),
+                new Attachment("image 2", 34),
+                new Attachment("image 1", 100))
+        );
+        Comparator comparator = new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Attachment left = (Attachment) o1;
+                Attachment right = (Attachment) o2;
+                return left.getSize() - right.getSize();
+            }
+        };
+        attachments.sort(comparator);
+        assertThat(attachments, is(expected));
+    }
+
+    @Test
+    public void whenGeneralizedAbstractComparatorForName() {
+        List<Attachment> attachments = new ArrayList<>(Arrays.asList(
+                new Attachment("image 3", 79),
+                new Attachment("image 1", 42),
+                new Attachment("image 2", 34))
+        );
+        List<Attachment> expected = Arrays.asList(
+                new Attachment("image 1", 42),
+                new Attachment("image 2", 34),
+                new Attachment("image 3", 79)
+        );
+        Comparator comparatorGen = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment o1, Attachment o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+        attachments.sort(comparatorGen);
+        assertThat(attachments, is(expected));
+    }
 }
