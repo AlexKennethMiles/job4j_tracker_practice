147f464e4b184671e8783c8e3eb42fb26adc1559
AlexKennethMiles <apuchkov17@gmail.com>
2021-07-04 17:06:13 +0300

    1.2.(1) Функциональный интерфейс [#247497 #173959]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -26,7 +26,6 @@ public class FunctionalInterfaces {
         }
 
         Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
-//        List<String> strings = new ArrayList<>(map.values());
 
         Consumer<String> con = s -> System.out.println(s);
         Function<String, String> func = s -> s.toUpperCase();
