7fa38f356cd5bf4a3cde35a52c4429bc2d09be4a
AlexKennethMiles <apuchkov17@gmail.com>
2021-07-08 19:47:46 +0300

    6. Функции высшего порядка [#24255 #175525]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class PhoneDictionaryTest {
