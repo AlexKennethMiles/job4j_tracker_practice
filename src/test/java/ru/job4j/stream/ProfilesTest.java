6726abf31f2db8357ac64d3431e0e4ac5b72779b
AlexKennethMiles <apuchkov17@gmail.com>
2021-07-10 11:55:10 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -22,8 +22,30 @@ public class ProfilesTest {
         Profiles prof = new Profiles();
         List<Address> expected = new ArrayList<>();
         expected.add(new Address("London", "Baker Street", 221, 2));
-        expected.add(new Address("Moscow", "Bolshaya Sadovaya ulitsa", 10, 50));
         expected.add(new Address("Lyon", "rue Peyrat", 8, 1));
+        expected.add(new Address("Moscow", "Bolshaya Sadovaya ulitsa", 10, 50));
+        assertThat(prof.collect(profiles), is(expected));
+    }
+
+    @Test
+    public void collectAndSortAddress() {
+        List<Profile> profiles = List.of(
+                new Profile("Mikhail Bulgakov",
+                        new Address("Moscow", "Bolshaya Sadovaya ulitsa", 10, 50)),
+                new Profile("Sherlock Holmes",
+                        new Address("London", "Baker Street", 221, 2)),
+                new Profile("Dr John H. Watson",
+                        new Address("London", "Baker Street", 221, 2)),
+                new Profile("Antoine Marie Jean-Baptiste Roger vicomte de Saint-Exupéry",
+                        new Address("Lyon", "rue Peyrat", 8, 1)),
+                new Profile("Jean de Saint-Exupéry",
+                        new Address("Lyon", "rue Peyrat", 8, 1))
+        );
+        Profiles prof = new Profiles();
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("London", "Baker Street", 221, 2));
+        expected.add(new Address("Lyon", "rue Peyrat", 8, 1));
+        expected.add(new Address("Moscow", "Bolshaya Sadovaya ulitsa", 10, 50));
         assertThat(prof.collect(profiles), is(expected));
     }
 }
